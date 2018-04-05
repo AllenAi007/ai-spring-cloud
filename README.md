b6e7ec435f78bdb29e2aa616acf495ff475ee78c
aihua <13348867336@163.com>
2018-04-05 08:35:18 +0800

    add myservice and eureka

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ai-spring-cloud
 POC for spring cloud Netflix services
+
+# myservice 9000
+a sample micro service where provide a hello world: http://localhost:9000/hello
+
+# eureka 9001 
+Service Registration Server http://localhost:9001/
---
+++ myservice/src/main/java/com/ai/spring/cloud/myservice/MyServiceApplication.java
@@ -0,0 +1,24 @@
+package com.ai.spring.cloud.myservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * My Service Application
+ */
+//@EnableDiscoveryClient
+@RestController
+@SpringBootApplication
+public class MyServiceApplication {
+
+    @GetMapping("/hello")
+    String hello(){
+        return "Hello Spring cloud!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyServiceApplication.class, args);
+    }
+}
