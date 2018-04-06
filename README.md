b809cbf41584c98d05e3ae4db7ce42ffe830b01e
aihua <13348867336@163.com>
2018-04-06 12:23:18 +0800

    add router and filter zuul

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ a sample micro service where provide a hello world: http://localhost:9000/hello
 
 # eureka 9001 
 Service Registration Server http://localhost:9001/
+
+# router and filter zuul 9002
+gateway zuul - http://localhost:9002/api/ms/hello
+api/ms/** -> map to myservice/**
--- myservice/src/main/java/com/ai/spring/cloud/myservice/MyServiceApplication.java
+++ myservice/src/main/java/com/ai/spring/cloud/myservice/MyServiceApplication.java
@@ -2,19 +2,25 @@ package com.ai.spring.cloud.myservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * My Service Application
  */
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @RestController
 @SpringBootApplication
 public class MyServiceApplication {
 
+    @GetMapping("/")
+    String home() {
+        return hello();
+    }
+
     @GetMapping("/hello")
-    String hello(){
+    String hello() {
         return "Hello Spring cloud!";
     }
 
